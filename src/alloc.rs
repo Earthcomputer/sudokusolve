cb750a730836e439d0801d628515b87c68b1381d
Joe <burtonjae@hotmail.co.uk>
2022-10-13 20:52:41 +0100

    Add explanatory comments to Z3Allocator

--- src/alloc.rs
+++ src/alloc.rs
@@ -2,6 +2,9 @@ use std::cell::UnsafeCell;
 use std::marker::PhantomData;
 
 pub struct Z3Allocator<T> {
+    // This type is covariant, but without erased pointers the compiler doesn't detect it as such.
+    // Covariance is important for self-referential structs that may contain this allocator.
+    // We also use a cell because &mut Z3Allocation<T> is invariant.
     things: UnsafeCell<Vec<*mut ()>>,
     _phantom: PhantomData<T>,
 }
@@ -19,6 +22,7 @@ impl<T> Z3Allocator<T> {
 
     pub fn alloc(&self, thing: T) -> &T {
         let heap = Box::leak(Box::new(thing));
+        // Safety: we are !Send and !Sync so there are guaranteed to be no data races.
         unsafe { &mut *self.things.get() }.push(heap as *mut T as *mut ());
         heap
     }
@@ -27,6 +31,7 @@ impl<T> Z3Allocator<T> {
 impl<T> Drop for Z3Allocator<T> {
     fn drop(&mut self) {
         for thing in self.things.get_mut() {
+            // Safety: These pointers come from Box::leak in the alloc method.
             unsafe {
                 drop(Box::from_raw(*thing as *mut T));
             }
